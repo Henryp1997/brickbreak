2c80d8752a18bd71f9cbda011ba8ce8de4306206
Henry Pickersgill <htap2@live.co.uk>
2024-02-09 18:47:26 +0000

    fix padlocked brick collision problems

--- objects.py
+++ objects.py
@@ -263,7 +263,7 @@ class brick():
         # generate_powerup = 0
         if generate_powerup == 0 and self.health < 3:
             power_type = all_powerup_types[list(all_powerup_types.keys())[random.randint(0, len(all_powerup_types.keys()) - 1)]][0]
-            # power_type = 'multi'
+            # power_type = 'laser'
             power_up = powerup(pos[0], pos[1], True, power_type)
             all_powerups.append(power_up)
         return
@@ -423,15 +423,24 @@ class laser():
                 if abs(self.y - b) < 10:
                     if l <= self.x <= r:
                         bricks_hit = 1
-                        brick_obj.health -= 1
-                        if brick_obj.health == 0:
-                            play_sound("smash")
-                            brick_obj.is_alive = False
-                            brick_obj.generate_powerup(all_powerups)
-                            all_bricks.pop(i)
-                        else:
+                        if brick_obj.health < 3:
+                            brick_obj.health -= 1
+                            if brick_obj.health == 0:
+                                play_sound("smash")
+                                brick_obj.is_alive = False
+                                brick_obj.generate_powerup(all_powerups)
+                                all_bricks.pop(i)
+                            else:
+                                play_sound("wall")
+                            return "dead"
+                        elif brick_obj.health == 3:
                             play_sound("wall")
-                        return "dead"
+                            return "dead"
+                        if len(all_bricks) == 1:
+                            locked_brick = all_bricks[0]
+                            if locked_brick.health == 3:
+                                # turn the padlocked brick into a normal 2-health brick
+                                locked_brick.health -= 1
 
 def play_sound(sound):
     pg.mixer.Sound.play(pg.mixer.Sound(f"{assets_path}/{sound}.wav"))
